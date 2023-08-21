fdba4cced2157e8813ad9089743911c4948adc29
rithik gundeti <rithikgundeti@gmail.com>
2023-08-21 15:54:49 +0530

    Commented predict.py

--- predict.py
+++ predict.py
@@ -1,3 +1,4 @@
+# Import necessary libraries
 import os
 import cv2
 import numpy as np
@@ -7,14 +8,18 @@ from ctpn import config
 from ctpn.ctpn import CTPN_Model
 from ctpn.utils import gen_anchor, transform_bbox, clip_bbox, filter_bbox, nms, TextProposalConnectorOriented
 
-# Loading the pre-trained model
+# Check if GPU is available, otherwise use CPU
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+# Loading pre-trained model
 weights = './weights/ctpn.pth'
 model = CTPN_Model().to(device)
 model.load_state_dict(torch.load(weights, map_location=device)['model_state_dict'])
 model.eval()
 
-def get_text_boxes(image, display=True, prob_thresh=0.3):
+# Function to detect text boxes
+def get_text_boxes(image, display=True, prob_thresh=0.4):
+    # Get image dimensions
     h, w = image.shape[:2]
     
     # Resize the image
@@ -24,13 +29,17 @@ def get_text_boxes(image, display=True, prob_thresh=0.3):
         w = int(w / rescale_fac)
         image = cv2.resize(image, (w, h))
     
+    # Create a copy of the image for visualization
     image_c = image.copy()
+    
+    # Normalizing the image
     image = image.astype(np.float32) - config.IMAGE_MEAN
-    image = torch.from_numpy(image.transpose(2, 0, 1)).unsqueeze(0).float().to(device) # Making it into a pt tensor and adding extra dim for batch inputs
+    image = torch.from_numpy(image.transpose(2, 0, 1)).unsqueeze(0).float().to(device)
     
     with torch.no_grad():
-        cls, regr = model(image)   # return classification score(probabilities) and reg values
-        cls_prob = F.softmax(cls, dim=-1).cpu().numpy()
+        # Pass the image to the model
+        cls, regr = model(image)                            # return classification scores and reg values
+        cls_prob = F.softmax(cls, dim=-1).cpu().numpy()     # gets the probabilites by applying softmax on classification values
         regr = regr.cpu().numpy()
         
         # Generate anchor boxes and transform them
@@ -56,22 +65,29 @@ def get_text_boxes(image, display=True, prob_thresh=0.3):
         select_anchor = select_anchor[keep]
         select_score = select_score[keep]
 
+        # Use the TextProposalConnectorOriented class to connect text lines
         textConn = TextProposalConnectorOriented()
         text = textConn.get_text_lines(select_anchor, select_score, [h, w])
         
+        # Draw bounding boxes and annotations on the image
         if display:
             for i in text:
-                s = str(round(i[-1] * 100, 2)) + '%'
                 i = [int(j) for j in i]
                 cv2.line(image_c, (i[0], i[1]), (i[2], i[3]), (0, 0, 255), 2)
                 cv2.line(image_c, (i[0], i[1]), (i[4], i[5]), (0, 0, 255), 2)
                 cv2.line(image_c, (i[6], i[7]), (i[2], i[3]), (0, 0, 255), 2)
                 cv2.line(image_c, (i[4], i[5]), (i[6], i[7]), (0, 0, 255), 2)
-                #cv2.putText(image_c, s, (i[0] + 13, i[1] + 13), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
         
+        # Return detected text lines and the image with bounding boxes
         return text, image_c
 
-img_path = 'images/006.jpg'
+
+# Load the input image
+img_path = 'images/download.jpeg'
 input_img = cv2.imread(img_path)
+
+# Get detected text lines and image with bounding boxes
 text, out_img = get_text_boxes(input_img)
+
+# Save the image with drawn bounding boxes
 cv2.imwrite('res.jpg', out_img)
